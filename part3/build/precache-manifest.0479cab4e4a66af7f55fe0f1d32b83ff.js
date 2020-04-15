420cf71fad07708246a722436c1b11e8e6199224
Tomasz <sieczko.tomasz@gmail.com>
2020-04-14 21:24:19 -0700

    Finished phonebook backend step 9

---
+++ part3/build/precache-manifest.0479cab4e4a66af7f55fe0f1d32b83ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f8ff0bb58a41614e5998628b68d70a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1120c1a899b0c52f9a5c",
+    "url": "/static/js/main.788d5be2.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- part3/index.js
+++ part3/index.js
@@ -96,6 +96,7 @@ app.post('/api/persons', (request, response) => {
 	response.json(newPerson)
 })
 
-app.listen(3001, () => {
-	console.log('Server running on port 3001')
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
