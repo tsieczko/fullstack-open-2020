ab7f27ddf97de48063c43e1e1f8251c71885ddbe
Tomasz <sieczko.tomasz@gmail.com>
2020-04-15 20:22:38 -0700

    Finished phonebook database step3

--- part3/index.js
+++ part3/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const Person = require('./models/person')
 
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
+app.use(cors())
 
 morgan.token('body', (req, res) => {
 	return JSON.stringify(req.body)
@@ -22,30 +24,6 @@ app.use(morgan((tokens, req, res) => {
 		tokens.body(req, res)
 	].join(' ')
 }))
-
-
-let persons = [
-	{
-		"name": "Arto Hellas",
-		"number": "040-123456",
-		"id": 1
-	},
-	{
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523",
-		"id": 2
-	},
-	{
-		"name": "Dan Abramov",
-		"number": "12-43-234345",
-		"id": 3
-	},
-	{
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122",
-		"id": 4
-	}
-]
 
 app.get('/api/persons', (request, response) => {
 	Person.find({})
@@ -74,10 +52,13 @@ app.get('/info', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
 	const id = request.params.id
-	persons = persons.filter(person => {
-		return person.id != id
-	})
-	response.status(204).end()
+	Person.findByIdAndRemove(id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => {
+			console.log('how', error)
+		})
 })
 
 app.post('/api/persons', (request, response) => {
--- part3/models/person.js
+++ part3/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 const url = process.env.MONGODB_URI
 
@@ -16,6 +17,14 @@ mongoose.connect(url, {
 const personSchema = new mongoose.Schema({
 	name: String,
 	number: String
+})
+
+personSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Person', personSchema)
