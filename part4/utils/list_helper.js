510b0ae99433b00967b95d97b6ca5563474bdc02
Tomasz <sieczko.tomasz@gmail.com>
2020-04-17 17:18:22 -0700

    Finished helper functions and unit tests step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	if (blogs.length == 0) {
+		return 0
+	}
+
+	return blogs.reduce((acc, cur) => {
+		return {
+			likes: acc.likes + cur.likes
+		}
+	}).likes
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
